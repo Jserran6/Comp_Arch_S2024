9a93daf8182409fe2438c4d36aada06f5a68b3ea
root <Jserran6@stedwards.edu>
2024-02-01 21:07:57 +0000

    initial part 2 commit

--- Unit2_Assembly_Basics/part2_exercises.asm
+++ Unit2_Assembly_Basics/part2_exercises.asm
@@ -10,11 +10,24 @@
 
 %include "asm_io.inc"
 
+age equ 30 ; symbol
+%define fav_number 14 ; macro
+
 ; initialized data
 segment .data
+fav_color db "purple", 0 ; string
+least_fav_color db "yellow", 0
+number dd 116 ; integer
+letter db "A" ; character
+num dd 74 
+hello_world db "Hello World!", 0
+
+char_prompt db "Enter a character: ", 0
+int_prompt db "Enter a intiger: ", 0
 
 ; uninitialized data
 segment .bss
+space_for_int resd 1 ; space for i integer
 
 
 segment .text
@@ -23,7 +36,34 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+	mov eax, [number] ; eax = 116
+        mov dword [number], 32
+
+        ; call print_int
+        ; call print_char 
+
+        ; mov eax, [num]
+        ; call print_int
+        ; call print_nl
+        ; call print_char
+
+        ; dump_mem 1, fav_color, 0
+
+        ; mov eax, fav_color + 3 ; skips first 3 letters
+        ; call print_string
+
+        ; mov eax, hello_world
+        ; call print_string
+        ; call print_nl
+        ; dump_mem 1, hello_world, 0
+
+        mov eax, char_prompt
+        call print_string
+        call read_char
+
+        mov eax, int_prompt
+        call print_string
+        call read_int
 
         popa
         mov     eax, 0            ; return back to C
